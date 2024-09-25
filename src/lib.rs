f3ea8ed704cb96735282727ffda0e74fb565a2b6
Hans Kratz <hans@appfour.com>
2024-09-25 12:16:21 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -35,13 +35,23 @@ pub fn expect(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    use proc_macro2::TokenStream;
-    use quote::quote;
-    let attr = TokenStream::from(attr);
-    let item = TokenStream::from(item);
-    let expanded = quote! {
-        #[expect(#attr)]
-        #item
-    };
-    expanded.into()
+    use proc_macro::{Delimiter, Group, Ident, Punct, Spacing, Span, TokenStream, TokenTree};
+    use std::iter::once;
+    let mut s = TokenStream::new();
+    s.extend(once(TokenTree::from(Punct::new('#', Spacing::Alone))));
+    let mut attr_inner = TokenStream::new();
+    attr_inner.extend(once(TokenTree::from(Ident::new(
+        "allow",
+        Span::call_site(),
+    ))));
+    attr_inner.extend(once(TokenTree::from(Group::new(
+        Delimiter::Parenthesis,
+        attr,
+    ))));
+    s.extend(once(TokenTree::from(Group::new(
+        Delimiter::Bracket,
+        attr_inner,
+    ))));
+    s.extend(item);
+    s
 }
