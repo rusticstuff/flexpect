e88a69c968ae6fa8d3b2c7063978c567b1e1dabf
Hans Kratz <hans@appfour.com>
2024-09-26 10:36:19 +0200

    ci work

--- tests/compare-output.sh
+++ tests/compare-output.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 command=$1
 expected_file=$2
-output=$(cargo ${command} 2>&1)
+output=$(cargo ${command} --all-targets --quiet 2>&1)
 echo "$output" > output.txt
 if ! diff -u ${expected_file} output.txt; then
 echo "Cargo ${command} output does not match expected output"
