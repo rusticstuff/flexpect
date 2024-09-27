605fe21fe42b071b68aae052782b5af12f133b19
Hans Kratz <hans@appfour.com>
2024-09-27 07:04:56 +0200

    remove special treatment for old nightlies

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate proc_macro;
 
-#[rustversion::any(before(1.43.0), all(nightly, before(2020-02-27)))]
+#[rustversion::any(before(1.43.0))]
 #[proc_macro_attribute]
 pub fn expect(
     _attr: proc_macro::TokenStream,
@@ -10,9 +10,7 @@ pub fn expect(
     item
 }
 
-#[rustversion::all(
-    any(since(1.43.0), all(nightly, since(2020-02-27))),
-    any(before(1.81), all(nightly, before(2024-06-27))))]
+#[rustversion::all(since(1.43.0), before(1.81))]
 #[proc_macro_attribute]
 pub fn expect(
     attr: proc_macro::TokenStream,
@@ -21,7 +19,7 @@ pub fn expect(
     replace_attr_name_with(attr, item, "allow")
 }
 
-#[rustversion::any(since(1.81), all(nightly, since(2024-06-27)))]
+#[rustversion::since(1.81)]
 #[proc_macro_attribute]
 pub fn expect(
     attr: proc_macro::TokenStream,
@@ -30,7 +28,7 @@ pub fn expect(
     replace_attr_name_with(attr, item, "expect")
 }
 
-#[rustversion::any(since(1.43.0), all(nightly, since(2020-02-27)))]
+#[rustversion::since(1.43.0)]
 fn replace_attr_name_with(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
