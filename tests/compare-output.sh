86cc986f424251d9fb1469a38de2e0af25542fbb
Hans Kratz <hans@appfour.com>
2024-09-27 06:01:18 +0200

    run submod test individually

--- tests/compare-output.sh
+++ tests/compare-output.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 command=$1
 expected_file=$2
-output=$(cargo ${command} -j 1 --all-targets --color=never --quiet 2>&1)
+output=$(cargo ${command} --color=never --quiet 2>&1)
 echo "$output" > /tmp/output.txt
 if ! diff -u ${expected_file} /tmp/output.txt; then
 echo "Cargo ${command} output does not match expected output"
